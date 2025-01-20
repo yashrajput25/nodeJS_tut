1a314afc61b69b9064912775efd96931ab570bdd
Yash Rajput <yashrajputyr10@gmail.com>
2025-01-20 10:42:11 +0530

    code for fs sync

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -3,6 +3,5 @@ console.log("Starting");
 
 const content = fs.readFileSync('./content/first.txt', 'utf-8');
 console.log('File Content:', content)
-
 console.log("Printed after the content is read through 'sync' ")
 
